826ce8fa8ad5ffdd32249160dfb0ec05666aa2fe
Akhilesh Pedgaonkar <pedgaonkar@wisc.edu>
2021-12-16 21:10:07 -0600

    remove first last index checking

--- eikonal_update.py
+++ eikonal_update.py
@@ -3,20 +3,8 @@
 import numpy as np
 
 def eikonal_quad_eq_solver(idx, jdx, u, Nt, F, h):
-    if (idx == 0):
-        U_H = min(u[1, jdx], 0)
-    elif (idx == Nt - 1):
-        U_H = min(u[Nt - 2, jdx], 0)
-    else:
-        U_H = min(u[idx - 1, jdx], u[idx + 1, jdx])
-
-    if (jdx == 0):
-        U_V = min(u[idx, 1], 0)
-    elif (jdx == Nt - 1):
-        U_V = min(u[idx, Nt - 2], 0)
-    else:
-        U_V = min(u[idx, jdx - 1], u[idx, jdx + 1])
-
+    U_H = min(u[idx - 1, jdx], u[idx + 1, jdx])
+    U_V = min(u[idx, jdx - 1], u[idx, jdx + 1])
     if (abs(U_H - U_V) > h / F[idx, jdx]):
         u[idx, jdx] = min(U_H, U_V) + h / F[idx, jdx]
     else:
